4697c90ad44f0a4e362dc253057d4aeba2567ea6
Đức Thiên Trần <thiencole16102006@gmail.com>
2026-08-31 21:28:48 +0800

    Complete Level 3 mark as done

--- src/main/java/Cole.java
+++ src/main/java/Cole.java
@@ -4,7 +4,7 @@ public class Cole {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         String line = "____________________________________________________________";
@@ -25,19 +25,43 @@ public class Cole {
 
             } else if (input.equals("list")) {
                 System.out.println(line);
+                System.out.println("Here are the tasks in your list:");
 
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
 
                 System.out.println(line);
 
+            } else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                Task task = tasks[taskNumber - 1];
+
+                task.markAsDone();
+
+                System.out.println(line);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + task);
+                System.out.println(line);
+
+            } else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7));
+                Task task = tasks[taskNumber - 1];
+
+                task.markAsNotDone();
+
+                System.out.println(line);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + task);
+                System.out.println(line);
+
             } else {
-                tasks[taskCount] = input;
+                Task task = new Task(input);
+                tasks[taskCount] = task;
                 taskCount++;
 
                 System.out.println(line);
-                System.out.println("added: " + input);
+                System.out.println("added: " + task);
                 System.out.println(line);
             }
         }
