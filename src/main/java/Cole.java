f56b6e99f75006fc7fa6aed81c675e76241d9891
Đức Thiên Trần <thiencole16102006@gmail.com>
2026-08-31 21:15:21 +0800

    Complete Level 1 echo

--- src/main/java/Cole.java
+++ src/main/java/Cole.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class Cole {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
         System.out.println("Hello! I'm Cole");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
